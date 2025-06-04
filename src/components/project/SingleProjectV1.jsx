0d1a9b6f87c2377a551c1bd31d74067e82182fc2
melvintivane <melvintivane@gmail.com>
2025-06-04 10:26:38 +0200

    refactor: remove unncessary deps

--- src/components/project/SingleProjectV1.jsx
+++ src/components/project/SingleProjectV1.jsx
@@ -1,32 +1,26 @@
-import { HashLink as Link } from 'react-router-hash-link';
-import styled from 'styled-components';
-
-const StyledImage = styled.img`
-    height: 100%;
-    width: 100%;
-    object-fit: cover;
-`
+import { HashLink as Link } from "react-router-hash-link";
 
 const SingleProjectV1 = ({ project }) => {
-    const { id, thumb, projectLink, subTitle, title, shape } = project;
+  const { id, thumb, projectLink, subTitle, title, shape } = project;
 
-    return (
-        <>
-            <div className="swiper-slide">
-                <div className="project-style-one">
-                    <StyledImage src={`/img/portfolio/${thumb}`} alt="Thumb" />
-                    {/*<img src={`/img/portfolio/${thumb}`} alt="Thumb" />*/}
-                    <div className="overlay">
-                        <span>{subTitle}</span>
-                        <h4><Link to={`/${projectLink}/${id}#`}>{title}</Link></h4>
-                    </div>
-                    <div className="shape">
-                        <img src={`/img/shape/${shape}`} alt="shape" />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="swiper-slide">
+        <div className="project-style-one">
+          <img src={`/img/portfolio/${thumb}`} alt="Thumb" />
+          <div className="overlay">
+            <span>{subTitle}</span>
+            <h4>
+              <Link to={`/${projectLink}/${id}#`}>{title}</Link>
+            </h4>
+          </div>
+          <div className="shape">
+            <img src={`/img/shape/${shape}`} alt="shape" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SingleProjectV1;
