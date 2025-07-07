51f4abea4b0d01e6df2b8fef3f63914c082c77a3
edmilson-saute <edmilsonsaute33@gmail.com>
2025-07-07 10:10:03 +0200

    fix: Corrige texto do botão ao mandar mensagem pelo form

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -177,8 +177,8 @@ const ContactForm = () => {
           <div className="row">
             <div className="col-lg-12">
               <button disabled={isSubmitting} type="submit" name="submit" id="submit">
-              <i className="fa fa-paper-plane"></i> {t.submitButton}
-               {isSubmitting ? 'Sending...' : ''}
+              <i className="fa fa-paper-plane"></i> 
+               {isSubmitting ?   t.loaderMessage : t.submitButton}
               </button>
             </div>
           </div>
